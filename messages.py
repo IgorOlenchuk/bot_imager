b06621b26cc048348d8de42ed0481e3981602dbc
Igor <olenchuk.io@yandex.ru>
2022-07-03 14:49:02 +0000

    final

--- messages.py
+++ messages.py
@@ -7,24 +7,24 @@ add_tg ={'RU': emoji.emojize(':paperclip:Добавить Olenchuk_Imager в с�
          'EN': emoji.emojize(':paperclip:Add Olenchuk_Imager to your chat')}
 lang = {'RU': emoji.emojize('🇷🇺 Выбор языка'),
         'EN': emoji.emojize('🏴󠁧󠁢󠁥󠁮󠁧󠁿Language selection')}
-start = {'RU': emoji.emojize('👋 Привет! Меня зовут Olencuhk_Imager.\n' \
+start = {'RU': emoji.emojize('👋 Привет! Меня зовут Bot_imager.\n' \
                 'Я - Бот для создания веб-скриншотов.\n' \
-                'Чтобы получить скриншот - отправьте URL адрес сайта. Например, https://wikipedia.org.\n' \
+                'Чтобы получить скриншот - отправьте URL адрес сайта. Например, olenchuk.ru.\n' \
                 '• С помощью бота вы можете проверять подозрительные ссылки. (Айпилоггеры, фишинговые веб-сайты, скримеры и т.п)\n' \
-                '• Вы также можете добавить меня в свои чаты, и я смогу проверять ссылки, которые отправляют пользователи.\n' \
-                'Olencuhk_Imager. использует geckodriver.\n' \
+                '• Вы также можете добавить меня в свои чаты, и я смогу проверять ссылки, которые отправляют пользователи (в разработке).\n' \
+                'Bot_imager. использует geckodriver.\n' \
                 'Работает с протоколами https.\n' \
                 'И находится в постоянной разработке.\n'),
-        'EN': emoji.emojize('👋 Hello! My name is Olencuhk_Imager.\n' \
+        'EN': emoji.emojize('👋 Hello! My name is Bot_imager.\n' \
                 'I am a bot for creating web screenshots.\n' \
-                'To get a screenshot - send the URL of the site. For example, https://wikipedia.org.\n ' \
+                'To get a screenshot - send the URL of the site. For example, olenchuk.ru.\n ' \
                 '• You can use the bot to check suspicious links. (Ipilloggers, phishing websites, screamers, etc.)\n' \
-                '• You can also add me to your chats and I will be able to check the links that users send.\n' \
-                'Olencuhk_Imager. uses geckodriver.\n' \
+                '• You can also add me to your chats and I will be able to check the links that users send (in progress).\n' \
+                'Bot_imager. uses geckodriver.\n' \
                 'Works with https protocols.\n' \
                 'And is in constant development.\n')}
-help = {'RU': 'Чтобы начать работу, просто напишите ссылку на сайт в формате: yandex.ru',
-        'EN': 'To get started, just write a link to the site in the format: yandex.ru'}
+help = {'RU': 'Чтобы начать работу, просто напишите ссылку на сайт в формате: olenchuk.ru',
+        'EN': 'To get started, just write a link to the site in the format: olenchuk.ru'}
 no_imput ={'RU': 'Вы не ввели адрес сайта!',
            'EN': 'You have not entered the website address!'}
 domen_free ={'RU': 'Домен свободен!',
