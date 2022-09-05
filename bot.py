d58448041cb5e707f719910dafe395f71224f845
IgorOlenchuk <olenchuk.io@yandex.ru>
2022-09-05 11:13:47 +0300

    captcha

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
-captcha_manager = CaptchaManager(bot.get_me(id), default_timeout=90)
+captcha_manager = CaptchaManager(bot.get_me(), default_timeout=90)
 
 # Message handler for new chat members
 @dp.message_handler(content_types=["new_chat_members"])
